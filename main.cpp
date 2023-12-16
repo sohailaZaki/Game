a7ba1e28cc4f5723f64ed149ec0cefc568af93a2
sohaila <sohailazaki@gmail.com>
2023-12-16 02:16:04 +0200

    editing .cpp file

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include<deque>
+#include<queue>
+#include <stdlib.h>
+#include <string>
+#include <stack>
+#include <algorithm>
+void main() {
+	cout << "Hello World";
+	cout << "Hello World 2";
+}
