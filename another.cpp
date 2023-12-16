0bdc9485da4b1056195ba80cb97474ac5f643637
sohaila <sohailazaki@gmail.com>
2023-12-16 02:30:32 +0200

    initial commit

---
+++ another.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include<deque>
+#include<queue>
+#include <stdlib.h>
+#include <string>
+#include <stack>
+#include <algorithm>
+int main() {
+	cout << "initial";
+	
+}
